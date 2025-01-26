715c2ef2770c03216800d2ca287a08cda185d3fa
imrahn <35425886+ImrahnF@users.noreply.github.com>
2025-01-26 18:18:26 -0500

    Update Item.hpp

--- Assignment1/Assignment1/Item.hpp
+++ Assignment1/Assignment1/Item.hpp
@@ -20,18 +20,21 @@ private:
 public:
     // Constructor
     Item(int id, const string& name, int quantity, double price)
-    : id(id), name(name), quantity(quantity), price(price) {}
-    
+        : id(id), name(name), quantity(quantity), price(price) {
+    }
+
     // Getters
     int getId() const { return id; }
     const string& getName() const { return name; }
     int getQty() const { return quantity; }
     double getPrice() const { return price; }
-    
-    // Display method (no need for a seperate cpp file), wouldn't really be used.
-    virtual void display() {
-        cout << "[" << getId() << "] " << getName() << " | Qty: " << getQty() << "x @$" << getPrice() << endl;
-    }
+
+    // Display method
+    virtual void display() = 0;
+
+    // These are used to help to format the derived item to be properly written to a text file
+    virtual int getPerishability() = 0;
+    virtual string getSpecificAttribute() = 0;
 };
 
 #endif /* Item_hpp */
