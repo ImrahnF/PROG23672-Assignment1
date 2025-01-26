984aad0a32f411797b3a1158cd0243f73a4e41e5
imrahn <35425886+ImrahnF@users.noreply.github.com>
2025-01-26 18:19:30 -0500

    Update NonPerishableItem.hpp

--- Assignment1/Assignment1/NonPerishableItem.hpp
+++ Assignment1/Assignment1/NonPerishableItem.hpp
@@ -9,6 +9,7 @@
 #define NonPerishableItem_hpp
 
 #include <iostream>
+#include <string>
 
 class NonPerishableItem : public Item {
 private:
@@ -16,14 +17,18 @@ private:
 public:
     // Constructor
     NonPerishableItem(int id, const string& name, int quantity, double price, int warrantyPeriod)
-    : Item(id, name, quantity, price), warrantyPeriod(warrantyPeriod) {}
+        : Item(id, name, quantity, price), warrantyPeriod(warrantyPeriod) {
+    }
     const int getWarrantyPeriod() const { return warrantyPeriod; }
-    
+
     // Display method including warranty period
     void display() override {
         cout << "[" << getId() << "] " << getName() << " | Qty: " << getQty() << "x @$" << getPrice() <<
-        " (Warranty period: " << getWarrantyPeriod() << " days)" << endl;
+            " (Warranty period: " << getWarrantyPeriod() << " days)" << endl;
     }
+
+    int getPerishability() override { return 0; }
+    string getSpecificAttribute() override { return to_string(warrantyPeriod); }
 };
 
 #endif /* NonPerishableItem_hpp */
